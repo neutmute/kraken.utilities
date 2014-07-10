69da179bf10ed368184cfe91d1ce9ea3abb17b41
neutmute <neutmute@users.noreply.github.com>
2014-07-10 20:49:10 +1000

    tests

---
+++ source/_Tests/Kraken.Core.Tests/Core/Extensions/StringExtensionsFixture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Kraken.Core.Extensions;
+using Kraken.Core.Tests;
+using NUnit.Framework;
+
+namespace Kraken.Core.Tests.Business.ExtensionMethods
+{
+
+    public class StringExtensionsFixture : Fixture
+    {
+        [Test]
+        public void EqualsCaseInsensitive()
+        {
+            Assert.That("123".EqualsCaseInsensitive("123"));
+            Assert.That("".EqualsCaseInsensitive(""));
+            Assert.That(((string)null).EqualsCaseInsensitive(null));
+            Assert.That("Foo".EqualsCaseInsensitive("foo"));
+
+            Assert.That(!("Foo1".EqualsCaseInsensitive("foo")));
+            Assert.That(!((string)null).EqualsCaseInsensitive("klkl"));
+        }
+
+        [Test]
+        public void TrimToMax()
+        {
+            var input = "123456789";
+            Assert.AreEqual("12...", input.TrimToMax(5));
+            Assert.AreEqual("12345", input.TrimToMax(5, StringExtensions.TrimToMaxOptions.NoElipsis));
+        }
+    }
+}
