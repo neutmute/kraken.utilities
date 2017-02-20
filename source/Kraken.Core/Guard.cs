3235f77608fb8dfab99f634db499ed36ec2e6385
neutmute <neutmute@users.noreply.github.com>
2017-02-20 22:29:51 +1100

    guard

--- source/Kraken.Core/Guard.cs
+++ source/Kraken.Core/Guard.cs
@@ -7,12 +7,54 @@ namespace Kraken.Core
 {
     public static class Guard
     {
-        public static void NullArgument(string name, object argument)
+        public static void NullOrEmpty(string value, string messageFormat = null, params object[] messageArgs)
         {
-            if (argument == null)
+            if (string.IsNullOrEmpty(value))
             {
-                throw new ArgumentNullException(name);
+                throw KrakenException.Create(GetMessage("String cannot be null or empty", messageFormat, messageArgs));
             }
+        }
+
+        public static void EnumIsZero(Enum value)
+        {
+            if (Convert.ToInt32(value) == 0)
+            {
+                throw KrakenException.Create("Cannot be zero");
+            }
+        }
+
+        public static void Against(bool conditionThatShouldBeFalse, string messageFormat = null, params object[] messageArgs)
+        {
+            if (conditionThatShouldBeFalse)
+            {
+                throw KrakenException.Create(messageFormat, messageArgs);
+            }
+        }
+
+        public static void That(bool conditionThatShouldBeTrue, string messageFormat = null, params object[] messageArgs)
+        {
+            if (!conditionThatShouldBeTrue)
+            {
+                throw KrakenException.Create(messageFormat, messageArgs);
+            }
+        }
+
+
+        public static void Null(object value, string messageFormat = null, params object[] messageArgs)
+        {
+            if (value == null)
+            {
+                throw KrakenException.Create(GetMessage("object cannot be null", messageFormat, messageArgs));
+            }
+        }
+
+        private static string GetMessage(string defaultMessage, string messageFormat, params object[] messageArgs)
+        {
+            if (messageFormat != null)
+            {
+                defaultMessage = string.Format(messageFormat, messageArgs);
+            }
+            return defaultMessage;
         }
     }
 }
---
+++ source/_Tests/Kraken.Core.Tests/Core/GuardFixture.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Kraken.Core.Tests.TestClasses;
+using NUnit.Framework;
+
+namespace Kraken.Core.Tests.Core
+{
+    public class GuardFixture : Fixture
+    {
+        [ExpectedException(typeof(KrakenException))]
+        [Test]
+        public void EnumZero()
+        {
+            Guard.EnumIsZero(Colour.Unknown);
+        }
+
+        [ExpectedException(typeof(KrakenException))]
+        [Test]
+        public void That()
+        {
+            var value = -1;
+            Guard.That(value > 0, "WTF?");
+        }
+
+        [ExpectedException(typeof(KrakenException))]
+        [Test]
+        public void Against()
+        {
+            var uninitialised = int.MinValue;
+            Guard.Against(uninitialised == int.MinValue, "WTF?");
+        }
+
+        [Test]
+        public void EnumOk()
+        {
+            Guard.EnumIsZero(Colour.Red);
+            // shouldn't throw
+        }
+
+
+        [Test]
+        [ExpectedException(typeof(KrakenException), ExpectedMessage = "taskInfo.InputParam is required")]
+        public void NullOrEmpty()
+        {
+            Guard.NullOrEmpty("", "taskInfo.InputParam is required");
+        }
+
+    }
+}
