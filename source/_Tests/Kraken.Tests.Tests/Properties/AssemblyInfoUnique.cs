6c1b6fc853aabc715d93a4a539282225c064d00c
neutmute <neutmute@users.noreply.github.com>
2017-09-05 21:19:42 +1000

    more standard 2.0

--- source/_Tests/Kraken.Tests.Tests/Properties/AssemblyInfoUnique.cs
+++ source/_Tests/Kraken.Tests.Tests/Properties/AssemblyInfoUnique.cs
@@ -1,12 +1,4 @@
 ﻿using System.Reflection;
-using Kraken.Core;
 
 [assembly: AssemblyTitle("Kraken.Core.Tests")]
 [assembly: AssemblyDescription("Utilities")]
-
-#if DEBUG
-[assembly: AssemblyCompilation(BuildConfiguration.Debug)]
-#else
-[assembly:  AssemblyCompilation(BuildConfiguration.Release)]
-#endif
-
